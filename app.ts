2f233a4321007b4535a2dfe1cfb052e8cf1036e7
ollivarila <olli.varila@gmail.com>
2023-05-10 23:01:39 +0300

    Fix health checks

--- app.ts
+++ app.ts
@@ -1,3 +1,13 @@
 import MyClient from './MyClient'
+import http from 'http'
 const client = new MyClient()
 client.start()
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World\n')
+})
+
+server.listen(8080, () => {
+  console.log('Application running')
+})
